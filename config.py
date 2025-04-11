2a23aad243f8fa591b74d8208eddb1b372bde39e
Biotr <piotrekkielbowicz@gmail.com>
2025-04-11 22:07:16 +0200

    changed config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_key = "c0dc53bc4cc0b599616d037ebbaacb7cdf0cdba7b4f6a2776ba27c77cb551854"  # Get your api key
-api_secret = "b4efcb99996605858096bf3f370745bf191e9631072e6bf9863b4ac1d0e4ee4e"  # Get your api key
+api_key = "api_key"  # Get your api key
+api_secret = "api_secret"  # Get your api key
 leverage = 2
 trading_view_pairs = ["XRPUSDT"]  # Available pairs exampe: ["XRPUSDT","ETHUSDT","BTCUSDT"]
 discord_pairs = ["ETHUSDT"]  # Available pairs
